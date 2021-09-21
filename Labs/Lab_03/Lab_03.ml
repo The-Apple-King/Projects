83c6ee0babc5f45536ebc4a07db54596310f8187
Owen Swearingen <SWEAR041@umn.edu>
2021-09-21 12:13:02 -0500

    generic

--- Labs/Lab_03/Lab_03.ml
+++ Labs/Lab_03/Lab_03.ml
@@ -1,9 +1,27 @@
-let rec sumf (xs: float list): float =
+let rec sum (xs: float list) =
 match xs with
-| [] -> 0.0
+| [] -> []
 | x :: rest -> x +. sumf rest
 
-let rec square (xs: float list): float =
+let rec square (vs: float list)=
+match vs with
+| [] -> []
+| v :: rest -> (v*.v)::square rest
+
+let rec exclamify (ps: string list): string list =
+match ps with
+| [] -> ["!"]
+| p :: rest -> (p ^ "!") :: exclamify rest
+
+
+let rec remove_odds (xs: int list) =
 match xs with
-| [] -> 0.0
-| x :: rest -> (x *. x) + square rest
+|[]->[]
+|x:: rest -> if x mod 2 = 0 then x :: remove_odds rest else remove_odds rest
+
+
+(*let remove_caps (xs: string list) =
+match xs with
+|[] -> []
+|x :: rest -> if (equal (uppercase_ascii (get x 0)) (get x 0)) then exclamify rest
+ *)

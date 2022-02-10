587b46f3663a1a91748728240446ab65f492b06e
Owen Swearingen <swear041@umn.edu>
2022-02-09 19:17:53 -0600

    finished c++ lab

--- csci1113/Labs/lab3/stretch.cpp
+++ csci1113/Labs/lab3/stretch.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main() {
 
 	double cost = 0;
-    int paid = 0;
+    double paid = 0;
     int change = 0;
     cout << "Enter cost: ";
     cin >> cost;
     cout << "Enter paid in Dollars: ";
     cin >> paid;
 
-    change = (int)(100*(paid- cost)+1);
-    cout << change;
+    change = (100*paid) - (100*cost);
+    cout << change/2000 << " 20 Dollar Bills" << "\n";
+    change = change%2000;
+    cout << change/1000 << " 10 Dollar Bills" << "\n";
+    change = change%1000;
     cout << change/100 << " Dollars" << "\n";
     change = change%100;
     cout << change/25 << " Quarters" << "\n";
--- csci1113/Labs/lab3/warmup.md
+++ csci1113/Labs/lab3/warmup.md
@@ -2,14 +2,14 @@
 
 2. 1, 0
    1, 0
-   0, 0
+   2, 0
 
 3. if(exams >= 60 && programsDone >= 10)
    cout >> "Passed"
    else
    cout >> "Failed"
 
-4. if temperature >= 100 || pressure >= 200)
+4. if(temperature >= 100 || pressure >= 200)
    cout >> "Warning"
    else
    cout >> "OK"

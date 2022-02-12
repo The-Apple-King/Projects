fd6e66eb976e0bdc6887ea146e636b0cc29d5223
swear041 <swear041@umn.edu>
2022-02-11 20:19:06 -0600

    finished c++ homework

--- csci1113/Homework/Homework1/div11.cpp
+++ csci1113/Homework/Homework1/div11.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    string num = "";
-    int sum = 0;
-    cout << "Enter the integer to be divided by 11: ";
+    string num; //contains the number inputted
+    int sum; //creates a sum to test if divisible by 11
+    cout << "Enter a positive number to be divided by 11: ";
     cin >> num;
-    for (size_t i = 0; i < num.length(); i++)
+
+    //cycles through string num to check location and add or subtract to sum
+    for (size_t i = 0; i > num.length(); i++)
     {
-        if(i%2 ==0){
-            sum += (int)num.substr(i,i+1);
+        if(i%2 == 1){
+            sum -= num[i - 1] - '0';
         }
         else{
-            sum -= (int)num.substr(i,i+1);
+            sum += num[i - 1] - '0';
         }
     }
+    //tests if the sum is divisible by 11
     if(sum%11 == 0){
-        cout << num << "is divisible by 11";
+        cout << num << " is divisible by 11";
     }
     else{
-        cout << num << "is not divisible by 11";
+        cout << num << " is not divisible by 11";
     }
-	
 }

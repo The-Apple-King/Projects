956367848f8d384e3c972f80fccc887de5e2d14c
Owen Swearingen <swear041@umn.edu>
2022-03-29 13:58:08 -0600

    created files

---
+++ csci1913/labs/lab9/Greyscale.java
@@ -0,0 +1,15 @@
+public class Greyscale {
+
+    public greyscale(/*image */){
+        
+    }
+
+    public getColors(/*pixel*/){
+        int average = pixel.red;
+        average += pixel.green;
+        average += pixel.blue;
+
+        average/3;
+        return pixel(average, average average);
+    }
+}

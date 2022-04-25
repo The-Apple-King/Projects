1a27a68db24654740f9172909790858e3c80a68e
swear041 <swear041@umn.edu>
2022-04-24 19:49:59 -0500

    finished c++ 7 homework

--- csci1113/Homework/Homework7/hw7b.cpp
+++ csci1113/Homework/Homework7/hw7b.cpp
@@ -7,26 +7,26 @@ using namespace std;
 class PointMass
 {
 private:
-    double loc[3];
-    double mass;
+    double loc[3]; // x, y, z
+    double mass;// mass of point object
 
 public:
-    PointMass();
-    PointMass(double, double, double, double);
-    double getX();
-    double getY();
-    double getZ();
-    double getMass();
+    PointMass(); //default constructor
+    PointMass(double, double, double, double); //constructor
+    double getX(); //return x
+    double getY(); //return y
+    double getZ(); //return z
+    double getMass(); // return mass
 };
 
-PointMass centerOfMass(PointMass* points, double numPoints);
-ostream &operator<<(ostream &out, PointMass &a);
+PointMass centerOfMass(PointMass* points, double numPoints); // takes an array of pointmass objects and finds center of mass
+ostream &operator<<(ostream &out, PointMass &a); //outputs point objects
+istream &operator>>(istream &l, PointMass &r); // inputs for point objects
 
 int main()
 {
-
+    //create the ifstream and make sure it works
     ifstream infile;
-
     infile.open("pointMass.txt");
     if (!infile)
     {
@@ -34,40 +34,48 @@ int main()
         return -1;
     }
 
-    PointMass points[10];
-    double numPoints = 0;
+    //create the array of pointMasses
+    PointMass *points;
+    int numPoints = 0;
     infile >> numPoints;
     while (numPoints <= 0)// extra test in case negative or 0 points
     {
         infile >> numPoints;
     }
+
+    //creates puts data into the pointMass array
+    points = new PointMass[numPoints];
     for (size_t i = 0; i < numPoints; i++)
     {
-        double x,y,z,mass;
-        infile >> x >> y >> z >> mass;
-        points[i] = PointMass(x,y,z,mass);
+        infile >> points[i];
     }
     
+    //output for location of pointMasses
     cout << "Location matrix: " << endl;
     for (size_t i = 0; i < numPoints; i++)
     {
             cout << points[i].getX() << " " << points[i].getY() << " " << points[i].getZ() << endl;
     }
 
+    //output for masses of each pointMass 
     cout << "Masses vector: " << endl;
     for (size_t i = 0; i < numPoints; i++)
     {
         cout << points[i].getMass() << endl;
     }
+    //output for number of pointMasses
     cout << "N:\n" << numPoints << endl;
     
+    //output for center of masses
     PointMass COM = centerOfMass(points, numPoints);
     cout << "Center of Gravity: " << endl;
     cout << COM;
-
 }
 
-
+/**
+ * @brief Construct a new Point Mass:: Point Mass object
+ * 
+ */
 PointMass::PointMass()
 {
     for (size_t i = 0; i < 3; i++)
@@ -77,6 +85,14 @@ PointMass::PointMass()
     mass = 0;
 }
 
+/**
+ * @brief Construct a new Point Mass:: Point Mass object
+ * 
+ * @param x x value
+ * @param y y value
+ * @param z z value
+ * @param massval mass value
+ */
 PointMass::PointMass(double x, double y, double z, double massval)
 {
     loc[0] = x;
@@ -85,23 +101,53 @@ PointMass::PointMass(double x, double y, double z, double massval)
     mass = massval;
 }
 
+/**
+ * @brief returns x
+ * 
+ * @return double x val
+ */
 double PointMass::getX()
 {
     return loc[0];
 }
+
+/**
+ * @brief returns y
+ * 
+ * @return double y val
+ */
 double PointMass::getY()
 {
     return loc[1];
 }
+
+/**
+ * @brief returns z
+ * 
+ * @return double z val
+ */
 double PointMass::getZ()
 {
     return loc[2];
 }
+
+/**
+ * @brief returns mass
+ * 
+ * @return double mass val
+ */
 double PointMass::getMass()
 {
     return mass;
 }
 
+/**
+ * @brief finds the center of mass
+ * 
+ * @param points array of pointMass objects
+ * @param numPoints the size of pointMass array
+ * @return PointMass a pointmass of the center of the mass
+ */
 PointMass centerOfMass(PointMass* points, double numPoints){
     double numerator[3] = {0,0,0};
     double massSum = 0;
@@ -128,12 +174,30 @@ PointMass centerOfMass(PointMass* points, double numPoints){
     
 }
 
+/**
+ * @brief prints out pointMasses
+ * 
+ * @param out ostream
+ * @param a the pointMass
+ * @return ostream& the output
+ */
 ostream &operator<<(ostream &out, PointMass &a){
     out << a.getX() << " " << a.getY() << " " << a.getZ() ;
     return out;
 }
-
-//ifstream &operator>>(ifstream in, double x){}
+/**
+ * @brief takes input for point objects
+ * 
+ * @param l the istream
+ * @param r the pointmass object
+ * @return istream& the input for the pointmass object
+ */
+istream &operator>>(istream &l, PointMass &r) {
+    double x,y,z,mass;
+        l >> x >> y >> z >> mass;
+        r = PointMass(x,y,z,mass);
+        return l;
+}
 
 
 

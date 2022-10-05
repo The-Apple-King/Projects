f9f5225975c81f08e253e7404bfa84c2c96cd7e6
Owen Swearingen <swear041@umn.edu>
2022-10-05 13:25:39 -0500

    added the file to write

---
+++ csci2021/Projects/p2-code/batt_update.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "batt.h"
+#include <stdint.h>
+
+int batt_update();
+// Called to update the battery meter display.  Makes use of
+// set_batt_from_ports() and set_display_from_batt() to access battery
+// voltage sensor then set the display. Checks these functions and if
+// they indicate an error, does NOT change the display.  If functions
+// succeed, modifies BATT_DISPLAY_PORT to show current battery level.
+// 
+// CONSTRAINT: Does not allocate any heap memory as malloc() is NOT
+// available on the target microcontroller.  Uses stack and global
+// memory only.
+
+
+int set_batt_from_ports(batt_t *batt)
+{
+    //figure out how this works
+    batt->mlvolts = BATT_VOLTAGE_PORT;
+    batt->mode = BATT_STATUS_PORT;
+
+}
+// Uses the two global variables (ports) BATT_VOLTAGE_PORT and
+// BATT_STATUS_PORT to set the fields of the parameter 'batt'.  If
+// BATT_VOLTAGE_PORT is negative, then battery has been wired wrong;
+// no fields of 'batt' are changed and 1 is returned to indicate an
+// error.  Otherwise, sets fields of batt based on reading the voltage
+// value and converting to precent using the provided formula. Returns
+// 0 on a successful execution with no errors. This function DOES NOT
+// modify any global variables but may access global variables.
+//
+// CONSTRAINT: Avoids the use of the division operation as much as
+// possible. Makes use of shift operations in place of division where
+// possible.
+//
+// CONSTRAINT: Uses only integer operations. No floating point
+// operations are used as the target machine does not have a FPU.
+// 
+// CONSTRAINT: Limit the complexity of code as much as possible. Do
+// not use deeply nested conditional structures. Seek to make the code
+// as short, and simple as possible. Code longer than 40 lines may be
+// penalized for complexity.

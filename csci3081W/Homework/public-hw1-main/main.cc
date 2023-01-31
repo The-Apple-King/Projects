7669bf2d1fb8f1cc4f8153df5136030a875babde
Owen Swearingen <swear041@umn.edu>
2023-01-31 16:46:34 -0600

    finished child classes

--- csci3081W/Homework/public-hw1-main/main.cc
+++ csci3081W/Homework/public-hw1-main/main.cc
@@ -2,19 +2,20 @@
 #include <string>
 #include <vector>
 
-#include "duck.cc"
+#include <duck.cc>
 
 int main()
 {
     std::vector<Duck> ducks;
-    ducks.push_back(Duck(std::string("Fulvous Whistling Duck"), 8.6f, 1.6d));
-    ducks.push_back(Duck(std::string("Domestic Duck"), 2.5f, 2.5d));
-    ducks.push_back(Duck(std::string("CanvasBack"), 5.8f, 2.7d));
-    ducks.push_back(Duck(std::string("Loon"), 10.0f, 10.0d));
-    ducks.push_back(Duck(std::string("Mallard"), 7.6f, 2.5d));
+    ducks.push_back(Duck(std::string("Fulvous Whistling Duck"), 8.6f, 1.6));
+    ducks.push_back(Duck(std::string("Domestic Duck"), 2.5f, 2.5));
+    ducks.push_back(Duck(std::string("CanvasBack"), 5.8f, 2.7));
+    ducks.push_back(Duck(std::string("Loon"), 10.0f, 10.0));
+    ducks.push_back(Duck(std::string("Mallard"), 7.6f, 2.5));
 
     // TODO part A: sort the ducks based on coolness
     // Note: You may want to double check your work here by printing to standard output
+    
 
     // TODO part C: Create an instance of each of the children classes in the main function.
     // TODO part C: Comment out the ducks vector, all the push_backs, and sort from part A.

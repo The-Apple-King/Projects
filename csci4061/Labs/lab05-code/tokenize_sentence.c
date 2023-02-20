7ac95db0a4d50ff620530824a96faaa313e9e070
SWEAR041 <swear041@umn.edu>
2023-02-20 17:50:14 -0600

    finished lab

--- csci4061/Labs/lab05-code/tokenize_sentence.c
+++ csci4061/Labs/lab05-code/tokenize_sentence.c
@@ -1,33 +1,38 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 #define BUF_LEN 512
 
 void print_words(char *s) {
-    // TODO Not yet implemented
+  // TODO Not yet implemented
+  char *token = strtok(s, " ");
+  while (token) {
+    printf("%s\n", token);
+    token = strtok(NULL, " ");
+  }
 }
 
 int main(int argc, char **argv) {
-    char sentence[BUF_LEN];
+  char sentence[BUF_LEN];
 
-    printf("sentence> ");
-    while(fgets(sentence, BUF_LEN, stdin) != NULL) {
-        // Remove trailing newline from sentence
-        int i = 0;
-        while (sentence[i] != '\n') {
-            i++;
-        }
-        sentence[i] = '\0';
+  printf("sentence> ");
+  while (fgets(sentence, BUF_LEN, stdin) != NULL) {
+    // Remove trailing newline from sentence
+    int i = 0;
+    while (sentence[i] != '\n') {
+      i++;
+    }
+    sentence[i] = '\0';
 
-        if (strcmp(sentence, "exit") == 0) {
-            return 0;
-        }
-
-        print_words(sentence);
-
-        printf("sentence> ");
+    if (strcmp(sentence, "exit") == 0) {
+      return 0;
     }
 
-    printf("\n");
-    return 0;
+    print_words(sentence);
+
+    printf("sentence> ");
+  }
+
+  printf("\n");
+  return 0;
 }

69736afe4d4256cb699af2078b07ea0e526473c8
SWEAR041 <swear041@umn.edu>
2023-04-03 14:23:11 -0500

    finished part 1 of project 3

--- csci4061/Projects/proj3-code/part1/par_word_lengths.c
+++ csci4061/Projects/proj3-code/part1/par_word_lengths.c
@@ -18,7 +18,33 @@
  * Returns 0 on success or -1 on error.
  */
 int count_word_lengths(const char *file_name, int *counts) {
-    return 0;
+  FILE *fd = fopen(file_name, "r");
+  if (fd == NULL) {
+    perror("fopen");
+    return -1;
+  }
+  char test = 'a';
+  int count = 0;
+
+  // test if test is a letter if so count ++ when its a whitespace update counts
+  // and set count to 0
+  while (fread(&test, sizeof(char), 1, fd) == 1) {
+    if (isalpha(test)) {
+      count++;
+    } else if (isspace(test)) {
+      counts[count - 1]++;
+      count = 0;
+    }
+  }
+
+  if (ferror(fd)) {
+    perror("fread");
+    fclose(fd);
+    return -1;
+  }
+
+  fclose(fd);
+  return 0;
 }
 
 /*
@@ -30,22 +56,70 @@ int count_word_lengths(const char *file_name, int *counts) {
  * Returns 0 on success or -1 on error
  */
 int process_file(const char *file_name, int out_fd) {
-    return 0;
+  // create arr and set every val to 0
+  int counts[MAX_WORD_LEN];
+  memset(counts, 0, sizeof(counts));
+
+  // call this function
+  count_word_lengths(file_name, counts);
+
+  if (write(out_fd, counts, sizeof(int) * MAX_WORD_LEN) == -1) {
+    perror("write");
+    return -1;
+  }
+
+  return 0;
 }
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        // No files to consume, return immediately
-        return 0;
-    }
-
-    // TODO Create a pipe for child processes to write their results
-    // TODO Fork a child to analyze each specified file (names are argv[1], argv[2], ...)
-    // TODO Aggregate all the results together by reading from the pipe in the parent
-
-    // TODO Change this code to print out the total count of words of each length
-    for (int i = 1; i <= MAX_WORD_LEN; i++) {
-        printf("%d-Character Words: %d\n", i, -1);
-    }
+  if (argc == 1) {
+    // No files to consume, return immediately
     return 0;
+  }
+  // TODO Create a pipe for child processes to write their results
+  int fd[2];
+  pipe(fd);
+
+  // TODO Fork a child to analyze each specified file (names are argv[1],
+  // argv[2], ...)
+  // child handling
+  for (size_t i = 0; i < argc - 1; i++) {
+    pid_t child_pid = fork();
+    if (child_pid == 0) {
+      close(fd[0]);
+      process_file(argv[i + 1], fd[1]);
+      exit(0);
+    }
+  }
+  // parent handling
+  close(fd[1]);
+  int status;
+  for (size_t i = 0; i < argc - 1; i++) {
+    wait(&status);
+    if (WEXITSTATUS(status) != 0) {
+      close(fd[0]);
+      return -1;
+    }
+  }
+
+  // TODO Aggregate all the results together by reading from the pipe in the
+  // parent
+  int counts[MAX_WORD_LEN];
+  memset(counts, 0, sizeof(counts));
+  int current;
+  for (size_t i = 0; i < argc - 1; i++) {
+    for (size_t j = 0; j < MAX_WORD_LEN; j++) {
+      if(read(fd[0], &current, sizeof(int)) == -1){
+        perror("read");
+        return -1;
+      }
+      counts[j] += current;
+    }
+  }
+
+  // TODO Change this code to print out the total count of words of each length
+  for (int i = 1; i <= MAX_WORD_LEN; i++) {
+    printf("%d-Character Words: %d\n", i, counts[i-1]);
+  }
+  return 0;
 }

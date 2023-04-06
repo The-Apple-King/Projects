f76c083f11f36c724ba23222331700b0d4f31acc
SWEAR041 <swear041@umn.edu>
2023-04-06 15:11:30 -0500

    finished hw 3 for 4061

--- csci4061/Labs/lab10-code/udp_client.c
+++ csci4061/Labs/lab10-code/udp_client.c
@@ -2,30 +2,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define DEPT_LEN 5
 #define NAME_LEN 128
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("Usage: %s <Department> <Number>\n", argv[0]);
-        return 1;
-    }
-    // You may uncomment the following lines if you would like to use them
-    // const char *department = argv[1];
-    // int number = atoi(argv[2]);
+  if (argc != 3) {
+    printf("Usage: %s <Department> <Number>\n", argv[0]);
+    return 1;
+  }
+  // You may uncomment the following lines if you would like to use them
+  struct request {
+    char department[5];
+    int number;
+  };
 
-    // TODO Send a request for a course's title to UDP server
-    // You will need to use getaddrinfo() first
-    // Followed by the socket() system call
-    // And finally sendto() to send a request and recvfrom() to get the reply
+  struct response {
+    int lookUpStatus;
+    char courseName[128];
+  };
 
-    // Pay close attention to the request/response formats explained in
-    // QUESTIONS.txt -- you are encouraged to use structs to represent
-    // requests and responses
-    //
-    // Don't forget about endianness!
+  struct request rq;
+  memcpy(rq.department, argv[1], sizeof(char) * 5);
+  rq.number = atoi(argv[2]);
+  rq.number = htonl(rq.number);
+
+  struct response res;
+
+  // TODO Send a request for a course's title to UDP server
+  // You will need to use getaddrinfo() first
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_DGRAM;
+  struct addrinfo *server;
+
+  // set up addr info for socket
+  int ret_val = getaddrinfo("23.23.63.223", "4061", &hints, &server);
+  if (ret_val != 0) {
+    fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(ret_val));
+    return 1;
+  }
+
+  // Followed by the socket() system call
+  int sock_fd =
+      socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+  if (sock_fd == -1) {
+    perror("socket");
+    freeaddrinfo(server);
+    return 1;
+  }
+
+
+  // And finally sendto() to send a request and recvfrom() to get the reply
+  if (sendto(sock_fd, &rq, sizeof(struct request), 0, server->ai_addr,
+             server->ai_addrlen) == -1) {
+    perror("sendto");
+    freeaddrinfo(server);
+    close(sock_fd);
+    return 1;
+  }
+
+  // recvfrom
+  if (recvfrom(sock_fd, &res, sizeof(struct response), 0, NULL, NULL) == -1) {
+    perror("recvfrom");
+    close(sock_fd);
+    freeaddrinfo(server);
+    return 1;
+  }
+    res.lookUpStatus = htonl(res.lookUpStatus);
+
+  if (res.lookUpStatus == -1) {
+    printf("Course Not Found\n");
+    close(sock_fd);
+    freeaddrinfo(server);
+    return 1;
+  }
+
+  printf("Course Title: %s\n", res.courseName);
+
+  freeaddrinfo(server);
+  if (close(sock_fd) == -1) {
+    perror("close");
+    return 1;
+  }
+
+  // Pay close attention to the request/response formats explained in
+  // QUESTIONS.txt -- you are encouraged to use structs to represent
+  // requests and responses
+  //
+  // Don't forget about endianness!
+  return 0;
 }
--- csci4061/Projects/proj3-code/part2/swish_funcs.c
+++ csci4061/Projects/proj3-code/part2/swish_funcs.c
@@ -25,11 +25,170 @@
  * Returns 0 on success or -1 on error.
  */
 int run_piped_command(strvec_t *tokens, int *pipes, int n_pipes, int in_idx, int out_idx) {
-    // TODO Complete this function's implementation
+
+    //close all unused pipes
+    for (size_t i = 0; i < n_pipes*2; i++)
+    {
+        if(i != in_idx && i != out_idx){
+            if(close(pipes[i]) == -1){
+                return -1;
+            }
+        }
+    }
+
+    if(in_idx != -1){// set input if we need to
+        if(dup2(pipes[in_idx], STDIN_FILENO) == -1){
+            if(close(pipes[in_idx]) == -1){
+                return -1;
+            }
+            if(close(pipes[out_idx]) == -1){
+                return -1;
+            }
+            return -1;
+        }
+    }
+    if(out_idx != -1){// set output if we need to 
+        if(dup2(pipes[out_idx], STDOUT_FILENO) == -1){
+            if(close(pipes[in_idx]) == -1){
+                return -1;
+            }
+            if(close(pipes[out_idx]) == -1){
+                return -1;
+            }
+            return -1;
+        }
+    }
+
+    // run command
+    if(run_command(tokens) == -1){
+        if(close(pipes[in_idx]) == -1){
+                return -1;
+            }
+        if(close(pipes[out_idx]) == -1){
+                return -1;
+            }
+        return -1;
+    }
+
+    // close last two pipes before closing
+    if(in_idx != -1){
+        if(close(pipes[in_idx]) == -1){
+            return -1;
+        }
+    }
+    if(out_idx != -1){
+        if(close(pipes[out_idx]) == -1){
+            return -1;
+        }
+    }
     return 0;
 }
 
 int run_pipelined_commands(strvec_t *tokens) {
-    // TODO Complete this function's implementation
+    // find number of pipes
+    int n_pipes = strvec_num_occurrences(tokens, "|");
+
+    //create the token for a single command
+    strvec_t *cmd = malloc(sizeof(strvec_t));
+    if(cmd == NULL){
+        perror("malloc");
+        return -1;
+    }
+
+    // create pipes
+    int *fd = malloc(sizeof(int) * n_pipes * 2);
+    if(fd == NULL){
+        perror("malloc");
+        return -1;
+    }
+    // open pipes
+    for (size_t i = 0; i < n_pipes; i++)
+    {
+        if(pipe(fd+(i*2))){
+            perror("opening pipes");
+            free(cmd);
+            free(fd);
+            return -1;
+        }
+    } 
+
+    // set up bounds for single command
+    int start = tokens->length;
+    int end = tokens->length;
+
+    //loop to execute each command 
+    for (int i = n_pipes; i >= 0; i--)
+    {
+        if(i != n_pipes){
+        end = start;    // set end of tokens based on last start
+        }
+        start = strvec_find_last(tokens, "|"); // find the last | and then go one past to the command
+        // no error checking needed as we take advantage of returning -1
+        start++;// if no val 0 else we go one past the |
+
+        // calculate the input and output pipes
+        int input = -1;
+        int output = -1;
+        if(i != 0){
+            input = (i-1)*2; 
+        }
+        if(i != n_pipes){
+            output = (i*2)+1;
+        }
+
+        // take the last command only
+        if(strvec_slice(tokens, cmd, start, end) == -1) {
+            perror("strvec_slice");
+            strvec_clear(cmd);
+            free(cmd);
+            return -1;
+        }
+        // remove the last command
+        strvec_take(tokens, start-1);
+            
+        // branch off into a child and call run_piped_command
+        pid_t child_pid = fork();
+        if (child_pid == 0)
+        {   
+            // run the last command
+            int ret_val = run_piped_command(cmd, fd, n_pipes, input, output);
+
+            //free mem
+            strvec_clear(cmd);
+            free(cmd);
+            // we exit using ret_val main will error check using it
+            exit(ret_val);
+        }
+        // be sure to clear cmd when in main to be ready for next time
+        strvec_clear(cmd);
+    }
+
+    //close all remaining pipes
+    for (size_t i = 0; i < n_pipes*2; i++)
+    {
+        if(close(fd[i]) == -1){
+                return -1;
+        }
+    }
+    
+    
+    // wait for all children to complete
+    for (int i = n_pipes; i >= 0; i--) {
+        int status;
+        wait(&status);
+        // check that the children exited
+        if (WEXITSTATUS(status) != 0) {
+            perror("wait");
+            free(fd);
+            free(cmd);
+            return -1;
+        }
+    }
+
+    // final free
+    free(fd);
+    free(cmd);
+    
+    //return
     return 0;
 }

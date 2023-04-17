2a84b5a163e823393f2d8e669fa2534f8f0fa0c9
SWEAR041 <swear041@umn.edu>
2023-04-17 18:30:31 -0500

    finished lab12 4061

--- csci4061/Labs/lab12-code/queue.c
+++ csci4061/Labs/lab12-code/queue.c
@@ -8,12 +8,19 @@ int queue_init(queue_t *queue) {
     queue->size = 0;
 
     // TODO Initialize synchronization primitives here
+    pthread_mutex_init(&queue->lock, NULL);
+    pthread_cond_init(&queue->queue_full, NULL);
+    pthread_cond_init(&queue->queue_empty, NULL);
     return 0;
 }
 
 int queue_add(queue_t *queue, const char *item) {
     // TODO Add synchronization logic before and after queue
     // modification code below as needed
+    pthread_mutex_lock(&queue->lock);
+    while(queue->size == 5){
+        pthread_cond_wait(&queue->queue_full, &queue->lock);
+    }
 
     if (queue->head == NULL) {
         if ((queue->head = malloc(sizeof(node_t))) == NULL) {
@@ -41,6 +48,8 @@ int queue_add(queue_t *queue, const char *item) {
         queue->size++;
     }
 
+    pthread_cond_signal(&queue->queue_empty);
+    pthread_mutex_unlock(&queue->lock);
     return 0;
 }
 
@@ -48,11 +57,19 @@ int queue_remove(queue_t *queue, char *item_dest) {
     // TODO Add synchronization logic before and after queue
     // modification code below as needed
 
-    strcpy(item_dest, queue->head->message);
-    node_t *new_head = queue->head->next;
-    free(queue->head);
-    queue->head = new_head;
-    queue->size--;
+        pthread_mutex_lock(&queue->lock);
+        while(queue->size == 0){
+            pthread_cond_wait(&queue->queue_empty, &queue->lock);
+        }
+
+        strcpy(item_dest, queue->head->message);
+        node_t *new_head = queue->head->next;
+        free(queue->head);
+        queue->head = new_head;
+        queue->size--;
+
+        pthread_cond_signal(&queue->queue_full);
+        pthread_mutex_unlock(&queue->lock);
 
     return 0;
 }

ea8ad6cdab23c2303143fd52bb87e94dd2491e09
Khoa Tonthat <tonth005@umn.edu>
2023-04-26 00:47:56 -0500

    Implemented function definitions for the Chain Of Responsibility Strategy, Request and the logic of the Handlers still need to be implemented

--- libs/transit/include/ClosestHandler.h
+++ libs/transit/include/ClosestHandler.h
@@ -8,18 +8,43 @@
  */
 class ClosestHandler: public Handler{
     public:
+
         /**
          * @brief Construct a new Closest Handler object
          * 
          * @param next_handler pointer to next handler in the chain
          */
         ClosestHandler(Handler* next_handler = nullptr) : next_handler(next_handler){}
+        
+        /**
+        * @brief Checks to see if the handler can handle, if not pass request onto next handler
+        * 
+        * @param request 
+        * @return true if the handler can handle it
+        * @return false if the handler can't handle it
+        */
+        bool can_handle_request(Request& request)
+        
         /**
          * @brief Function to handle the request
          * 
          * @param request request to process
          */
         void handle_request(Request& request);
+
+        /**
+         * @brief Get the next handler object
+         * 
+         * @return Handler* 
+         */
+        Handler* get_next_handler();
+
+        /**
+         * @brief Set the next handler object
+         * 
+         * @param handler 
+         */
+        void set_next_handler(Handler* handler);
     private:
         Handler* next_handler;
 };
--- libs/transit/include/Handler.h
+++ libs/transit/include/Handler.h
@@ -10,14 +10,45 @@
  */
 class Handler{
     public:
+
+        /**
+         * @brief Checks to see if the handler can handle, if not pass request onto next handler
+         * 
+         * @param request 
+         * @return true if the handler can handle it
+         * @return false if the handler can't handle it
+         */
+        virtual bool can_handle_request(Request& request) = 0;
+
         /**
          * @brief Function to handle the request that is passed.
          * 
          * @param request Request to process
          */
         virtual void handle_request(Request& request) = 0;
+
+        /**
+         * @brief Get the next handler object
+         * 
+         * @return Handler* 
+         */
+       
+        virtual Handler* get_next_handler() = 0;
+        
+        /**
+         * @brief Set the next handler object
+         * 
+         * @param handler 
+         */
+        virtual void set_next_handler(Handler* handler) = 0;
+
+       
     private:
         Request* request;
+        Handler* next_handler;
+        
+        
+
 };
 
 #endif

3d6fb50e6bb96d0a3a3ee9e51e5a06db1a6bdba0
Alexander Tatley <tatle010@umn.edu>
2023-04-26 17:13:43 -0500

    Added relevant variables to track in csv to data for datacollection

--- libs/transit/include/Data.h
+++ libs/transit/include/Data.h
@@ -6,8 +6,8 @@
 #include <sstream>
 
 class Data {
-public:
 
+public:
     /**
      * @brief Construct a new Data:: Data object
      * 
@@ -15,9 +15,13 @@ public:
      * @param end end location of a trip
      * @param distance distance traveled by the drone
      * @param strategy movement strategy used
-     * @param deliveryType whether we moved a person or delivered food
+     * @param moneyMade how much money we made
+     * @param distanceFlown how much the drone flew on this recorded trip
+     * @param restaurantName name of restaurant we are performing an order for
+     * @param mealName name of the meal the drone is carrying
      */
-    Data(Vector3 start, Vector3 end, double distance, std::string strategy, std::string deliveryType);
+
+    Data(Vector3 start, Vector3 end, double distance, std::string strategy, double moneyMade, double distanceFlown, str::string restaurantName, str:string mealName);
 
     /**
      * @brief formats the data from the object into a csv friendly format
@@ -32,7 +36,11 @@ private:
     Vector3 end; // end position
     double distance; // distance traveled
     std::string strategy; // movement strategy
-    std::string deliveryType; // uber or uberEats
+    double moneyMade; // how much money has been made?
+    double distanceFlown; // how far did we fly on this trip?
+    str::string restaurantName; // what's the name of this restaurant?
+    str::string mealName; // what was the name of the meal we took?
+
 };
 
 #endif /* DATA_H */
--- libs/transit/src/Data.cc
+++ libs/transit/src/Data.cc
@@ -3,11 +3,7 @@
 #include <sstream>
 
 
-Data::Data(Vector3 start, Vector3 end, double distance, std::string strategy,
-           std::string deliveryType)
-    : start(start), end(end), distance(distance), strategy(strategy),
-      deliveryType(deliveryType) {}
-
+Data::Data(Vector3 start, Vector3 end, double distance, std::string strategy, double moneyMade, double distanceFlown, str::string restaurantName, str:string mealName) : start(start), end(end), distance(distance), strategy(strategy), moneyMade(moneyMade), distanceFlown(distanceFlown), restaurantName(restaurantName), mealName(mealName) {}
 
 std::string Data::toString(){
   std::ostringstream stream;
@@ -15,6 +11,10 @@ std::string Data::toString(){
   stream << end.x << "," << end.y << "," << end.z << ",";
   stream << distance << ",";
   stream << strategy << ",";
-  stream << deliveryType << "\n";
+  stream << moneyMade << ",";
+  stream << distanceFlown << ",";
+  stream << restaurantName << ",";
+  stream << mealName << ",";
+  // stream << deliveryType << "\n"; deliveryType is not defined yet
   return stream.str();
 }

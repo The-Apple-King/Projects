1a4d9b89189cf55b3058eb0a0e6f2b0168ff4454
Alexander Tatley <tatle010@umn.edu>
2023-04-26 19:30:23 -0500

    added outputFile line on DataCollection.cc

--- libs/transit/src/DataCollection.cc
+++ libs/transit/src/DataCollection.cc
@@ -18,7 +18,7 @@ void SimulationDataCollector::outputDataToCSV(std::string filename) {
     std::ofstream outputFile(filename);
 
     // Write the header row, will need to be changed for more stuff we add
-    outputFile << "Start x,y,z,End x,y,z,Distance,Strategy,Type of Delivery\n";
+    outputFile << "Start x,y,z,End x,y,z,Distance,Strategy, moneyMade, distanceFlown, restaurantName, mealName\n";
 
     // Write the data rows
     for (size_t i = 0; i < collectedData.size(); i++)
